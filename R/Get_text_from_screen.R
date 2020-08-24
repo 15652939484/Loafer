6e1d9ec3e66ffb8e82a5fae5073c16be4bebe375
Monkey. E. Luffy <13945755734@163.com>
2020-08-24 16:33:04 +0800

    Add files via upload
    Setup the project of build a package

---
+++ R/Get_text_from_screen.R
@@ -0,0 +1,27 @@
+#'redirect some obj(such as some model summary, which can be assigned directly.)
+#'this function redirect the text into a temp file and reread it.
+#'@param  my_message, obj print in the screen
+#' and can't be assigned to other variables directly.
+#'@examples
+#'\dontrun{data("iris")
+#'head(iris)
+#'z <- lm(Sepal.Length ~ Sepal.Width + Species, data = iris)
+#'z
+#'summary(z)
+#'k <- summary(z)
+#'k
+#'q <- Redirect_text_from_screen(summary(z))
+#'q
+#'}
+#'@return text by line. trans imformation into text and can be value assigned to other vars.
+
+Redirect_text_from_screen <-  function(my_message){
+  ### 从屏幕输出中获取结果。
+  ### 将结果文字返回。
+  sink(file = "temp.txt",append = F, type = "output")
+  print(my_message)
+  sink()
+  temp_df <-  read.delim("temp.txt",sep = "\n")
+  file.remove("temp.txt")
+  return(temp_df)
+}
---
+++ R/get_or_set_dir.R
@@ -0,0 +1,68 @@
+#' get or set the dir by Rstudio
+#' Get the dir where the script were lying in.
+#'     Set the dir as main_path and create some sub file folder inside the dir.
+#'     Chose one file as the new working dir.
+#'@param main_trunk_as_wd, Boolean, IF T, the main dir will be set as new wd.
+#'@param source_path, character, where raw data will be stored in a project.
+#'@param output_path, character, where the output of an analysis will be saved.
+#'@param other_dirs, character vector with var name. the name will be assigned and be returned to global env.
+#'@param setwd_in, name, source path default. when the main_trunk_as_wd is set as F, this var will be set as new wd.
+#'@return return a summary about working dir and the source and output dir.
+#'@examples
+#'\dontrun{# set the dir as wd:
+#'get_or_set_dir(main_trunk_as_wd = T,
+#'    source_path = "0_data_source/",
+#'    output_path = "2_output/",
+#'    other_dirs = c(function_path = "DIY Function/", other_dir_test = "Test dir"),
+#'    setwd_in = source_path)
+#'# set the source as wd:
+#' get_or_set_dir(main_trunk_as_wd = F,
+#'    source_path = "0_data_source/",
+#'    output_path = "2_output/",
+#'    other_dirs = c(function_path = "DIY Function/", Dir_test = "Test dir"),
+#'    setwd_in = source_path)
+#'}
+#'@export
+
+## export is required for the functions export to users.
+
+## Need: rstudioapi###
+get_or_set_dir <- function(main_trunk_as_wd = F,
+                           source_path = "0_data_source/",
+                           output_path = "2_output/",
+                           other_dirs = c(function_path = "DIY Function/"),
+                           setwd_in = source_path){
+  script_dir <- rstudioapi::getActiveDocumentContext()
+  main_path <<- sub("/[^/]+.R$", "/", script_dir[[2]])
+
+
+  if(main_trunk_as_wd == T){
+    setwd(main_path)
+  }else{
+    other_dirs <- c(source_path = source_path, output_path = output_path, other_dirs)
+  }
+
+  for(i in 1:length(other_dirs)){
+    path_tail <- other_dirs[i] %>% sub("([^/])$","\\1/",.) ## add a "/" tail in the path.
+    path <- paste0(main_path, path_tail)
+    ## create the path
+    path_create(path = path)
+    ### return the dirname to ClobalEnv.
+    try(assign(x = other_dirs[i] %>% names(), value = path, envir = .GlobalEnv))
+  }
+  if(main_trunk_as_wd == F & is.null(setwd_in) == F){
+    setwd(setwd_in)
+  }
+  ### 询问是否要自动替换工作目录.
+  cat("Working Dir was set to be ", getwd(), "\nsource_path  was", source_path, "\noutput_path was ", output_path, "\nAnd function_path was", function_path, "\t\n\nAll of them were renewed to function!!")
+} #### function 1 End #####
+
+#'check and create a path.
+#'@param path a path need to be checked.
+
+## no need to export, cause the useres didn't need this function.
+ path_create <- function(path){
+  if (dir.exists(path) == F) { dir.create(path, showWarnings = F, recursive = T) }
+}
+
+
