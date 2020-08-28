343dce894da39408c875ea73e660595cdd6b8674
Monkey. E. Luffy <13945755734@163.com>
2020-08-28 15:59:36 +0800

    清理多余文件
    在R中生成数据文件并且在包中/在用户环境使用的方法测试成功。
    在包中通过use_this包中的函数生成rda文件。
    在其他函数中直接使用数据集的名称调用即可。
    另外，用户可以通过data()对该数据集进行调用，也可以通过使用packagename:::data的方式进行调用。

--- data-raw/Get raw data.R
+++ data-raw/Get raw data.R
@@ -1,3 +1,5 @@
+### 用来生成相应的数据文件。通过 use_data进行保存即可。
+
 # source_path
 library(pacman)
 p_load(usethis)
@@ -6,11 +8,11 @@ p_load(openxlsx)
 # setwd(source_path)
 get_or_set_dir()
 getwd()
-setwd("J:/Github_Repository/Loafer/data-raw/0_data_source")
+# setwd("J:/Github_Repository/Loafer/inst/extdata")
 # pulled_df <- read.xlsx("希腊字母和英文读写的 对照表.xlsx")
 Greek_to_Enl_df <- read.xlsx("希腊字母和英文读写的 对照表.xlsx")
 # save(pulled_df,file = "data/Greek letters to English.rda")
 ## 不导出的数据
 usethis::use_data(Greek_to_Enl_df, overwrite = TRUE, internal = T)
-remove.packages("Loafer")
-p_load_current_gh("15652939484/Loafer")
+# remove.packages("Loafer")
+# p_load_current_gh("15652939484/Loafer")
